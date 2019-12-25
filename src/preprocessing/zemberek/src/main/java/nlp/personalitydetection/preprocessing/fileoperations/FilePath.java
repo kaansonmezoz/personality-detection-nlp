35ab889290654e042a882d6094c793d35d7c1bf6
kaansonmezoz <ksonmezoz@outlook.com>
2019-12-25 15:32:33 +0300

    Output path of Zemberek modified

--- src/preprocessing/zemberek/src/main/java/nlp/personalitydetection/preprocessing/fileoperations/FilePath.java
+++ src/preprocessing/zemberek/src/main/java/nlp/personalitydetection/preprocessing/fileoperations/FilePath.java
@@ -2,11 +2,10 @@ package nlp.personalitydetection.preprocessing.fileoperations;
 
 public class FilePath {
     public final static String ELIMINATED_DATASET_PATH = "/home/khan/Workspace/personality-detection-nlp/data/eliminated_data/eliminated_all_users_v2.csv";
-    public final static String PREPROCESSED_DATASET_OUTPUT_PATH = "/home/khan/Workspace/personality-detection-nlp/data/preprocessed_data/zemberek/zemberek_lemmatization.csv";
+    public final static String PREPROCESSED_DATASET_OUTPUT_PATH = "/home/khan/Workspace/personality-detection-nlp/data/preprocessed_data/zemberek/preprocessed_dataset_zemberek.csv";
     public final static String STOP_WORDS_PATH = "/home/khan/Workspace/personality-detection-nlp/data/stop_words_tr.txt";
     public final static String NORMALIZATION_LOOK_UP_PATH = "/home/khan/Workspace/personality-detection-nlp/src/preprocessing/dependencies/normalization";
     public final static String LM_FILE_PATH = "/home/khan/Workspace/personality-detection-nlp/src/preprocessing/dependencies/lm/lm.2gram.slm";
-
 
     private FilePath(){}
 }
